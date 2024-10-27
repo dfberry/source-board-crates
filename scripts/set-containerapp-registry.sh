8110192c976da69985d2047761342006683092ea
Dina Berry <dinaberry@outlook.com>
2024-10-27 20:15:37 +0000

    change containerapp registry

---
+++ scripts/set-containerapp-registry.sh
@@ -0,0 +1,6 @@
+az containerapp registry set \
+  -n my-containerapp \
+  -g MyResourceGroup \
+  --server MyExistingContainerappRegistry.azurecr.io \
+  --username MyRegistryUsername 
+  --password MyRegistryPassword
